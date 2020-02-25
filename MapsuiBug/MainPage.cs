215c93923b77e0c2998edcf83a929fd9b03151ab
pauldendulk <pauldendulk@gmail.com>
2020-02-25 08:48:11 +0100

    Adding files op full project

---
+++ MapsuiBug/MainPage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Xamarin.Forms;
+using SkiaSharp;
+
+namespace MapsuiBug
+{
+    public class MainPage : ContentPage
+    {
+        Button btnButton;
+
+        public MainPage() : base()
+        {
+            var mv = new SkiaSharp.Views.Forms.SKGLView();
+            mv.PaintSurface += Mv_PaintSurface;
+            mv.HorizontalOptions = LayoutOptions.FillAndExpand;
+            mv.VerticalOptions = LayoutOptions.FillAndExpand;
+            btnButton = new Button
+            {
+                Text = "Click Me"
+            };
+            btnButton.Clicked += BtnButton_Clicked;
+
+            Content = new StackLayout
+            {
+                Children =
+                    {
+                        mv,
+                        btnButton,
+                    }
+            };
+        }
+
+        private void Mv_PaintSurface(object sender, SkiaSharp.Views.Forms.SKPaintGLSurfaceEventArgs e)
+        {
+            e.Surface.Canvas.DrawRect(100, 100, 500, 500, new SKPaint { Color = SKColors.Pink });
+        }
+
+        private void BtnButton_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new SecondForm());
+        }
+    }
+}
---
+++ MapsuiBug/SecondForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace MapsuiBug
+{
+    class SecondForm : ContentPage
+    {
+        public SecondForm() : base()
+        {
+            this.Content = new Label
+            {
+                Text = "This is the second form. Now go back."
+            };
+        }
+    }
+}
