d2d34c67bca556c74d2d078081a9b360979174d9
pauldendulk <pauldendulk@gmail.com>
2020-02-26 07:29:23 +0100

    Add refresh button

--- MapsuiBug/MainPage.cs
+++ MapsuiBug/MainPage.cs
@@ -1,16 +1,20 @@
 ﻿using System;
 using Xamarin.Forms;
 using SkiaSharp;
+using SkiaSharp.Views.Forms;
+using System.Diagnostics;
 
 namespace MapsuiBug
 {
     public class MainPage : ContentPage
     {
         Button btnButton;
+        Button btnRefresh;
+        SKGLView mv;
 
         public MainPage() : base()
         {
-            var mv = new SkiaSharp.Views.Forms.SKGLView();
+            mv = new SKGLView();
             mv.PaintSurface += Mv_PaintSurface;
             mv.HorizontalOptions = LayoutOptions.FillAndExpand;
             mv.VerticalOptions = LayoutOptions.FillAndExpand;
@@ -20,19 +24,39 @@ namespace MapsuiBug
             };
             btnButton.Clicked += BtnButton_Clicked;
 
+            btnRefresh = new Button
+            {
+                Text = "Refresh"
+            };
+            btnRefresh.Clicked += BtnRefresh_Clicked; ;
+
             Content = new StackLayout
             {
                 Children =
                     {
                         mv,
-                        btnButton,
+                        new StackLayout 
+                        { 
+                            Orientation = StackOrientation.Horizontal,
+                            Children = 
+                                {                       
+                                   btnButton,
+                                   btnRefresh
+                                }
+                        }  
                     }
             };
+        }
+
+        private void BtnRefresh_Clicked(object sender, EventArgs e)
+        {
+            mv.InvalidateSurface();
         }
 
         private void Mv_PaintSurface(object sender, SkiaSharp.Views.Forms.SKPaintGLSurfaceEventArgs e)
         {
             e.Surface.Canvas.DrawRect(100, 100, 500, 500, new SKPaint { Color = SKColors.Pink });
+            Debug.WriteLine($"GRContext: {mv.GRContext}");
         }
 
         private void BtnButton_Clicked(object sender, EventArgs e)
